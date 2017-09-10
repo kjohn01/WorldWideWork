4cc0d0bb8f3d169f174ff59d9d99c2a142336610
KJOHN <mayacafe123@gmail.com>
2017-09-10 22:11:25 +0800

    gulp and img opt

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+new WOW().init();
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    var $cover = $("header");
+    var $top = $("#backToTop");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $cover.height());
+    $top.toggleClass('scrolled', $(this).scrollTop() > $cover.height());
+  });
+});
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
